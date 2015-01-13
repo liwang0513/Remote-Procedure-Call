472bc7eb882a00c5ff4c355ef85a1e821784e13b
liwang0513 <liwang0513@gmail.com>
2015-01-13 18:45:29 -0500

    add remove procedure call

--- rpc_code.php
+++ rpc_code.php
@@ -5,6 +5,26 @@
 
 require_once('Students.php');
 
+$student_data = new Students();
+
+if (isset($_POST['submit'])) {
+	
+	switch($_POST['request']) {
+		case "Get First Name":
+			$student_info = $student_data->getStudentFirstName();
+			break;
+		
+		case "Get Last Name":
+			$student_info = $student_data->getStudentLastName();
+			break;
+		
+		default:
+			http_response_code(400);
+	}
+}
+
+echo json_encode($student_info);
+
 ?>		
 
 <form action="rpc_code.php" method="post">
